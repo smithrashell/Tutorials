86511da75ef632ef4da166eb9a127feff143ff1a
Rashell Smith <rashellssmith@gmail.com>
2022-06-08 16:55:33 -0400

    second commit

---
+++ getting-started-tut/options.js
@@ -0,0 +1,43 @@
+
+
+let page = document.getElementByID("buttonDiv");
+let selectedClassName = "current"; 
+const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
+
+
+function handleButtonClick(event){
+
+	let current = event.traget.parentElement.querySelector(
+	`.${selectedVlassName}`
+	);
+
+	if(current && current !== event.target){
+	current.classList.remove(selectedClassName);
+	}
+
+	let color = event.target.dataset.color;
+	event.target.classList.add(selectedClassName);
+	chrome.storage.sync.set({color});
+
+  function costructOptions (buttonColors){
+		 chrome.storage.sync.get("color", (data)=>{
+		 let currentColor  = data.color;
+		 
+		 for(let buttonColor of ButtonColors){
+		 
+		 let button = document.createElement("button");
+		 button.dataset.color = buttonColor;
+		 button.style.backgroundColor = buttonColor; 
+		 
+		 if(buttonColor === currentColor){
+		 button.classList.add(selectedClassName)}
+		 }
+		 
+		 button.addEventListener("click",handleButtonClick);
+		 page.appendChild(button);
+	 }
+	 
+	 });
+
+
+}
